6f6d70acf58739e10510e4c31ad30f75495272cf
Erland Lewin <erland@lewin.nu>
2021-02-24 00:32:11 +0100

    Added simple scripts to read out Tesla battery module BMS slave data

---
+++ RaspberryPi/software/bms/index.js
@@ -0,0 +1,670 @@
+const SerialPort = require('serialport')
+
+class SerialWrapper
+{
+	constructor( device, speed )
+	{
+		this.device = device
+		this.speed = speed
+		this.readQueue = [];
+	}
+
+	async open()
+	{
+		return new Promise( (resolve, reject) => 
+			{
+				this.port = new SerialPort( this.device, { baudRate: this.speed }, err => 
+					{
+						if( err )
+							return reject( err );
+						else
+						{
+							this.port.on('error', function(err) {
+								console.log('Error: ', err.message)
+							})
+							/*
+							this.port.on('data', function (chunk) {
+							  console.log('onData!')
+							  // , port.read())
+							})
+							*/
+							this.port.on('readable', function () {
+								var readData 
+								  readData = this.port.read()
+								this.readQueue.push( ...readData )
+							}.bind(this))
+
+							return resolve( this );
+						}
+					});
+			} );
+	}
+
+	async write( buffer )
+	{
+		return new Promise( (resolve, reject) => 
+			{
+				this.port.write( buffer, '', err => { 
+					if(err)
+						reject( err );
+					else
+					{
+						this.port.drain( error => {
+							if(err) reject( err );
+							else
+								resolve();
+						});
+					}
+				});
+			});
+	}
+
+	readAll()
+	{
+		var result = this.readQueue;
+		this.readQueue = [];
+		return result;
+	}
+}
+
+
+class BMSBoard
+{
+	// TODO: Move to class for the TI BQ76PL536A-Q1 chip
+	// registers for bq76PL536A-Q1 (https://www.ti.com/lit/ds/symlink/bq76pl536a-q1.pdf)
+	static Registers = {
+		REG_DEV_STATUS: 0,
+		REG_GPAI:       1,
+		REG_VCELL1:         3,
+ 		REG_VCELL2:         5,
+		REG_VCELL3:         7,
+		REG_VCELL4:         9,
+		REG_VCELL5:         0xB,
+		REG_VCELL6:         0xD,
+		REG_TEMPERATURE1:   0xF,
+		REG_TEMPERATURE2:   0x11,
+		REG_ALERT_STATUS:   0x20,
+		REG_FAULT_STATUS:   0x21,
+		REG_COV_FAULT:      0x22,
+		REG_CUV_FAULT:      0x23,
+		REG_ADC_CONTROL:    0x30,
+		REG_IO_CONTROL:     0x31,
+		REG_BAL_CTRL:       0x32,
+		REG_BAL_TIME:       0x33,
+		REG_ADC_CONVERT:    0x34,
+		REG_ADDR_CTRL:      0x3B,
+		REG_RESET:	    0x3C,
+		REG_FUNCTION_CONFIG: 0x40
+	};
+
+	constructor( pack, id )
+	{
+		this.pack = pack;
+		this.id = id;
+		this.cellVoltages = [undefined, undefined, undefined, undefined, undefined, undefined]
+		this.temperatures = [undefined, undefined]
+	}
+
+	async readBytesFromRegister( register, byteCount )
+	{
+		return pack.readBytesFromDeviceRegister( this.id, register, byteCount );
+	}
+
+	async writeByteToRegister( register, byte )
+	{
+		console.log( "BMS Board: writeByteToRegister(register=" + register + ")" )
+
+		return pack.writeByteToDeviceRegister( this.id, register, byte )
+			.then( () => this.readFaults() )
+			.then( (faults) => console.log( "Faults: " + faults ) )
+	}
+
+	async writeAlertStatus( alertStatus )
+	{
+		return this.writeByteToRegister( BMSBoard.Registers.REG_ALERT_STATUS, 
+						 alertStatus.getByte() )
+	}
+
+	async readStatus()
+	{
+		var bytes = await this.readBytesFromRegister( BMSBoard.Registers.REG_ALERT_STATUS, 4 );
+		console.log( "status bytes=" + bytes );
+
+		this.alerts = new BQAlerts( bytes[0] ); 
+		console.log( "Alerts: " + this.alerts )
+		this.faults = new BQFaults( bytes[1] );
+		console.log( "Faults: " + this.faults )
+		this.covFaults = bytes[2];
+		this.cuvFaults = bytes[3];
+	}
+
+	async sleep()
+	{
+		// write 1 to IO_CONTROL[SLEEP] 
+		// turns off TS1, TS2, enter sleep mode
+		return this.writeIOControl( false, false, false, true, false, false )
+			// write 1 to ALERT[SLEEP]
+			.then( () => this.writeAlertStatus( BQAlerts.sleep ) ) // false, false, false, false, false, true, false, false ) )
+			// write 0 to ALERT[SLEEP]
+			.then( () => this.writeAlertStatus( BQAlerts.none ) ) // false, false, false, false, false, true, false, false ) )
+	}
+
+	async wake()
+	{
+		// write 0 to IO_CONTROL[SLEEP]
+		return this.writeIOControl( false, false, false, false, true, true )
+			.then( () => this.readIOControl() )
+			.then( (ioc) => console.log( "IOControl after wake: " + ioc ) );
+		// turn on TS1, TS2
+	}
+
+	async readValues()
+	{
+		//ADC Auto mode, read every ADC input we can (Both Temps, Pack, 6 cells)
+		//enable temperature measurement VSS pins
+		//start all ADC conversions
+		return this.writeADCControl( false, true, true, true, 6 )
+			.then( () => this.writeIOControl( false, false, false, false, true, true ) ) // wait one ms here?
+			.then( () => new Promise( resolve => setTimeout( () => resolve(), 1000 ) ) ) // waiting one second to test
+			.then( () => this.writeADCConvert( true ) )
+			.then( () => this.readMultiRegisters() )
+	}
+
+	async readConfig()
+	{
+		return this.readBytesFromRegister( BMSBoard.Registers.REG_FUNCTION_CONFIG, 8 )
+			.then( (bytes) => { console.log( "Configuration:" );
+					    for( var i = 0; i < 8; i++ )
+						console.log( i + ": " + bytes[i] );
+					});
+	}
+	
+	// async readVoltages()
+	// {
+	// 	var bytes = await this.readBytesFromRegister( BMSBoard.Registers.REG_ALERT_STATUS, 4 );
+	// }
+
+	async readMultiRegisters()
+	{
+		const bytes = await this.readBytesFromRegister( BMSBoard.Registers.REG_GPAI, 18 );
+		var tempTemp, tempCalc, logTempTemp;
+		var cellSum = 0
+
+		this.moduleVolt = (bytes[0] * 256 + bytes[1]) * 6.25 / (0.1875 * 2 ** 14); // 0.002034609;
+		console.log( "moduleVolt = " + this.moduleVolt + "(" + bytes[0] + ", " + bytes[1] + ")" );
+		
+		for( var i = 0; i < 6; i++ )
+		{
+			this.cellVoltages[i] = (bytes[2 + i * 2] * 256 + bytes[2 + i * 2 + 1]) * 6250 / (16383 * 1000)
+			cellSum += this.cellVoltages[i]
+			console.log( "Cell voltage " + i + " = " + this.cellVoltages[i] + "(" + bytes[2 + i * 2] + ", " + bytes[2 + i * 2 + 1] ) + ")"
+		}
+
+		console.log( "Sum of cells: " + cellSum )
+		tempTemp = (1.78 / ((bytes[14] * 256 + bytes[15] + 2) / 33046.0) - 3.57);
+		
+		tempTemp = tempTemp * 1000;
+		logTempTemp = Math.log( tempTemp );
+		tempCalc = 1.0 / (0.0007610373573 + (0.0002728524832 * logTempTemp) + (logTempTemp ** 3) * 0.0000001022822735);
+		this.temperatures[0] = tempCalc - 273.15
+		console.log( "Temp1 bytes=" + bytes[14] + ", " + bytes[15] + " = " + (bytes[14] * 256 + bytes[15]) )
+
+		// TODO: This is from Arduino code, the constant for division is different above and below, which is right? Also the +2 looks strange
+		tempTemp = (1.78 / ((bytes[16] * 256 + bytes[17] + 2) / 33068.0) - 3.57);
+		tempTemp = tempTemp * 1000;
+		logTempTemp = Math.log( tempTemp );
+		tempCalc = 1.0 / (0.0007610373573 + (0.0002728524832 * logTempTemp) + (logTempTemp ** 3) * 0.0000001022822735);
+		this.temperatures[1] = tempCalc - 273.15
+		console.log( "Temp1 bytes=" + bytes[16] + ", " + bytes[17] + " = " + (bytes[16] * 256 + bytes[17]) )
+
+		console.log( "Temperatures: " + this.temperatures[0] + ", " + this.temperatures[1] )
+	
+		//turning the temperature wires off here seems to cause weird temperature glitches
+	}
+
+	async readFaults()
+	{
+		return this.readBytesFromRegister( BMSBoard.Registers.REG_FAULT_STATUS, 1 )
+			.then( (bytes) => new BQFaults( bytes[0] ) )
+	}
+
+	async writeIOControl( auxConnected, gpioOutOpenDrain, gpioInHigh, sleep, ts1connected, ts2connected )
+	{
+		var value;
+
+		value = (auxConnected ? (1 << 7) : 0) |
+			(gpioOutOpenDrain ? (1 << 6) : 0) |
+			(gpioInHigh ? (1 << 5) : 0) |
+			(sleep ? (1 <<2) : 0) |
+			(ts2connected ? (1 << 1) : 0) |
+			(ts1connected ? 1 : 0);
+
+		console.log( "writeIOControl: ts1connected = " + ts1connected + ", value=" + value );
+		return this.writeByteToRegister( BMSBoard.Registers.REG_IO_CONTROL, value );
+	}
+
+	async readIOControl()
+	{
+		console.log( "readIOControl: entry" );
+		return this.readBytesFromRegister( BMSBoard.Registers.REG_IO_CONTROL, 1 )
+			.then( (bytes) => {console.log( "IOControl bytes=" + bytes ); return new BQIOControl( bytes[0] ) } )
+	}
+	
+	async writeADCControl( adcOn, tempSensor1On, tempSensor2On, gpaiOn, cellCount )
+	{
+		var value;
+
+		value = (adcOn ? (1 << 6) : 0) |
+			(tempSensor2On ? (1 << 5) : 0) |
+			(tempSensor1On ? (1 << 4) : 0) |
+			(gpaiOn ? (1 <<3) : 0);
+
+		if( cellCount > 1 && cellCount <= 6 )
+			value |= (cellCount - 1);
+		
+		console.log( "writeADCControl(tempSensor1On=" + tempSensor1On+ "), value=" + value.toString(2) );
+		return this.writeByteToRegister( BMSBoard.Registers.REG_ADC_CONTROL, value );
+	}
+
+	async writeADCConvert( initiateConversion )
+	{
+		return this.writeByteToRegister( BMSBoard.Registers.REG_ADC_CONVERT, initiateConversion ? 1 : 0 )
+	}
+
+	toString()
+	{
+		return "BMSBoard #" + this.id;
+	}
+}
+
+class BQIOControl
+{
+	constructor(byteValue) { this.byteValue = byteValue; }
+	toString()
+	{
+		var result = "IO Control: Aux: ";
+		if( this.byteValue & (1 << 7) )
+			result += " connected to REG50, ";
+		else
+			result += " Open, ";
+
+		result += "GPIO_out: ";
+
+		if( this.byteValue & (1 << 6) )
+			result += " open-drain, "
+		else
+			result += " output low, "
+
+		result += "GPIO_in: ";
+
+		if( this.byteValue & (1 << 5) )
+			result += " is high, "
+		else
+			result += " is low, "
+
+		result += "Sleep: ";
+
+		if( this.byteValue & (1 << 2) )
+			result += " Sleep mode, "
+		else
+			result += " Active mode, "
+
+		result += "TS2: "
+		if( this.byteValue & (1 << 1) )
+			result += " Connected"
+		else
+			result += " Not connected"
+
+		result += "TS1: "
+		if( this.byteValue & (1 << 0) )
+			result += " Connectetd"
+		if( this.byteValue == 0 )
+			result += " Not connected"
+
+		return result;
+	}
+}
+
+class BitmapField
+{
+	// maps bit to symbolic name
+	// { 0: 'jet engine enabled', 1: 'black hole collapsing' }
+	constructor( name, bits )
+	{
+		this.name = name;
+		this.bits = bits;
+		this.fields = {}
+		for( bit in bits )
+			this.fields[ this.bits[ bit ] ] = bit;
+	}
+
+	setFields( fieldValues, value )
+	{
+		for( field in fieldValues )
+		{
+			if( field in this.bitmap.fields )
+			{
+				var bit = this.bitmap.fields[field]
+				var mask = (1 << bit)
+
+				value = (value & ~mask) // reset bit
+				if( fieldValues[ field ] )
+					value = value | mask;
+			}
+			else
+				throw "Unrecognized field '" + field + "'"
+		}
+
+		return value;
+	}
+}
+
+class BitmapValue
+{
+	constructor( bitmap )
+	{
+		this.bitmap = bitmap;
+		this.value = 0;
+	}
+
+	setValue( value )
+	{
+		this.value = 0;
+	}
+
+	setFields( fields )
+	{
+		this.value = this.bitmap.setFields( fields, value )
+	}
+	getValue()	{ return this.value }
+	getFields()
+	{
+		return this.bitmap.getFields( value );
+	}
+
+	toString() { return "NIY"; }
+}
+
+class BQAlerts
+{
+	static sleep = new BQAlerts( 1 << 2 )
+	static none = new BQAlerts( 0 )
+/*
+	static createFrom( ar, parity, ecc_err, force, tsd, sleep, ot2, ot1 )
+	{
+		var byteValue = 0;
+		if( ar )
+			byteValue |= 1 << 7;
+		if( parity )
+			byteValue |= 1 << 6;
+		if( ecc_err )
+			byteValue |= 1 << 
+	}
+*/
+	constructor(byteValue) { this.byteValue = byteValue; }
+
+	getByte()
+	{
+		return this.byteValue;
+	}
+
+	toString()
+	{
+		var result = "Alerts: ";
+		if( this.byteValue & (1 << 7) )
+			result += " Address has not been assigned";
+		if( this.byteValue & (1 << 6) )
+			result += " Group 3 protected registers are invalid"
+		if( this.byteValue & (1 << 5) )
+			result += " Uncorrectable EPROM error"
+		if( this.byteValue & (1 << 4) )
+			result += " Alert asserted"
+		if( this.byteValue & (1 << 3) )
+			result += " Thermal shutdown"
+		if( this.byteValue & (1 << 2) )
+			result += " Sleep mode was activated"
+		if( this.byteValue & (1 << 1) )
+			result += " Overtemperature on TS2"
+		if( this.byteValue & (1 << 0) )
+			result += " Overtemperature on TS1"
+		if( this.byteValue == 0 )
+			result += " None"
+
+		return result;
+	}
+}
+
+class BQFaults
+{
+	constructor(byteValue) { this.byteValue = byteValue; }
+	toString()
+	{
+		var result = "Faults: ";
+		if( this.byteValue & (1 << 5) )
+			result += " Internal Consistency Check Failed"
+		if( this.byteValue & (1 << 4) )
+			result += " Fault Forced"
+		if( this.byteValue & (1 << 3) )
+			result += " Power-on-reset occurred"
+		if( this.byteValue & (1 << 2) )
+			result += " CRC error detected in last packet"
+		if( this.byteValue & (1 << 1) )
+			result += " Undervoltage"
+		if( this.byteValue & (1 << 0) )
+			result += " Overvoltage"
+		if( this.byteValue == 0 )
+			result += " None"
+
+		return result;
+	}
+}
+
+class BMSPack
+{
+	static MAX_MODULE_ADDR = 0x3e
+	static BROADCAST_ADDR = 0x3f
+ 
+	
+	constructor( serialDevice )
+	{
+		this.serial = new SerialWrapper(serialDevice, 612500 );
+		this.modules = {}
+	}
+
+	async init()
+	{
+		console.log( "Pack.init entry" );
+		await this.serial.open()
+			.then( () => { return this.findBoards() } )
+		console.log( "Pack.init exit" );
+	}
+
+	async findBoards()
+	{
+		var x;
+		var modules = []
+
+		console.log( "findBoards entry" );
+		for( x = 1; x < BMSPack.MAX_MODULE_ADDR; x++ )
+			await this.pollModule(x)
+				.then( module => {if( module ) { console.log( "module: " + module ); this.modules[ x ] = module; } } );
+		console.log( "findBoards exit" );
+		console.log( "this.moduls=" );
+		console.log( this.modules );
+	}
+
+	async renumberBoardIDs()
+	{
+	
+	}
+	
+	async wakeBoards()
+	{
+		return this.writeByteToDeviceRegister( BMSPack.BROADCAST_ADDR, BMSBoard.Registers.REG_IO_CONTROL, 0 )
+			.then( () => sleep(2) )
+			.then( () => this.checkAllStatuses() )
+			.then( () => this.readAllIOControl() )
+			.then( () => this.writeByteToDeviceRegister( BMSPack.BROADCAST_ADDR, BMSBoard.Registers.REG_ALERT_STATUS, 0x04 ) )
+			.then( () => sleep(2) )
+			.then( () => this.checkAllStatuses() )
+			.then( () => this.writeByteToDeviceRegister( BMSPack.BROADCAST_ADDR, BMSBoard.Registers.REG_ALERT_STATUS, 0 ))
+			.then( () => sleep(2) )
+			.then( () => this.checkAllStatuses() )
+			.then( () => console.log( "Boards should be awake" ) );
+	}
+
+	async checkAllStatuses()
+	{
+		for( var index in this.modules ) 
+		{ 
+			var faults = await this.modules[ index ].readStatus();
+			// console.log( "Module " + index + ": " + faults );
+		}
+	}
+	async readAllIOControl()
+	{
+		for( var index in this.modules ) 
+		{ 
+			var ioc = await this.modules[ index ].readIOControl();
+			console.log( "Module " + index + ": " + ioc );
+		}
+	}
+	
+	async pollModule(number)
+	{
+		var sendData = [number << 1,0,1] // bytes to send
+
+		await this.serial.write( sendData );
+		await sleep(20);
+		
+		var reply = this.serial.readAll();
+		if( reply.length > 4 )
+		{
+			console.log( "Found module #" + number + ": " + reply )
+			return new BMSBoard( this, number );
+		}
+		else
+		{
+			// console.log( "No module #" + number );
+			return null;
+		}
+	}
+
+	crc( data )
+	{
+		const generator = 0x07;
+		var crc;
+		var finalCRC;
+
+		finalCRC=data.reduce( (crc, byte) => { 
+			// console.log( "data.reduce: crc=0x" + crc.toString(16) + ", byte=0x" + byte.toString(16) );
+			crc = crc ^ byte;
+			// console.log( "After xor, crc=0x" + crc.toString(16) );
+			for( var i = 0; i < 8; i++ )
+			{
+				if( (crc & 0x80) != 0 )
+					crc = ((crc << 1) & 0xff) ^ generator;
+				else
+					crc = (crc << 1) & 0xff;
+				// console.log( "temp crc=0x" + crc.toString(16));
+			}
+			return crc;
+		}, 0x00 );
+
+		console.log( "CRC on " + data + " = 0x" + finalCRC.toString(16) );
+
+		return finalCRC
+	}
+
+	async readBytesFromDeviceRegister( device, register, byteCount )
+	{
+		var sendData = [device << 1,register,byteCount] // bytes to send
+
+		console.log( "sendData=" + sendData );
+
+		// TODO: add CRC check, retry on failed, return as soon as all data received
+		return this.serial.write( sendData )
+			.then( () => { return sleep( 20 /* 2*((byteCount + 4) / 8 + 1) */ ) } )
+			.then( () => {
+				var data = this.serial.readAll();
+				var crc = this.crc( data.slice(0,byteCount + 3 ) )
+				console.log( "Received data=" + data );
+				if( data.length == byteCount + 4 )
+				{
+					if( data[0] != sendData[0])
+						throw "first byte is " + data[0] + ", not device id " + device;
+					if( data[1] != register )
+						throw "second byte is " + data[1] + ", not register " + register;
+					if( data[2] != byteCount )
+						throw "third byte is " + data[2] + ", not byte count " + byteCount;
+					if( data[ data.length - 1 ] != crc )
+						throw "last byte is " + data[data.length-1] + ", not expected crc " + crc;
+					console.log( "readBytes... returning" );
+					return data.slice(3, 3 + byteCount);
+				}
+				else
+					throw "Expected " + (byteCount + 3) + " bytes, got " + data.length;
+			} )
+	}
+
+	async writeByteToDeviceRegister( device, register, byte )
+	{
+		var sendData = [ (device << 1) | 1, register, byte] // bytes to send
+		console.log( "writeBytesToDeviceRegister: register=" + register );
+
+		sendData.push( this.crc( sendData ) );
+		console.log( "writeBytes: sendData: " + sendData );
+		return this.serial.write( sendData )
+			.then( () => sleep( 2 * ( sendData.length / 8 + 1) ) )
+			.then( () => {
+				var reply = this.serial.readAll();
+				console.log( "writeBytes: received " + reply );
+
+				if( reply.length != sendData.length )
+					throw "Expected " + sendData.length + " bytes, got " + reply.length;
+				for( var i = 0; i < reply.length; i++ )
+					if( reply[i] != sendData[i] )
+						throw "Expected byte " + i + " to be " + sendData[i] + ", was " + reply[i]
+				})
+	}
+}
+
+var pack = new BMSPack("/dev/serial0");
+
+initPack( pack )
+	.then( () => pack.wakeBoards() )
+	.then( async () => 
+		{ 
+			console.log( "Checking statuses" );
+			console.log( pack.modules );
+			console.log( Object.entries( pack.modules ) );
+			// console.log( "Modules: " + JSON.stringify( pack.modules ) );
+			for( var key in pack.modules ) { 
+				console.log( "key: " + key );
+				var module = pack.modules[key];
+				console.log( "module: " + module);
+				await module.readIOControl()
+					.then( (ioControl) => {
+						console.log( ioControl )
+						return module.readStatus()
+						})
+					.then( () => { console.log( "Reading status..." ); return module.readStatus() } )
+					.then( () => { console.log( "Reading values..." ); return module.readValues() } )
+					.then( () => { console.log( "Reading config..." ); return module.readConfig() } )
+					.then( () => { console.log( "Sleeping module..." ); return module.sleep() } )
+					.then( () => { console.log( "Reading status again..." ); return module.readStatus() } )
+			}
+		}
+	)
+	.catch( (error) => { console.error( "ERROR: " + error ) } )
+
+async function initPack(pack)
+{
+	console.log( "before pack.init()" );
+	await pack.init();
+	console.log( "after pack.init()" );
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
