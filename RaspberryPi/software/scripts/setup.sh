d6958c3afa43f8472f0a4b0a08e731798dc23ea5
Erland Lewin <erland@lewin.nu>
2022-05-02 09:07:28 +0200

    Added reading battery fault line in scripts

--- RaspberryPi/software/scripts/setup.sh
+++ RaspberryPi/software/scripts/setup.sh
@@ -1,3 +1,4 @@
 sudo echo "10" >/sys/class/gpio/export
 sudo echo "22" >/sys/class/gpio/export
+sudo echo "25" >/sys/class/gpio/export
 
--- RaspberryPi/software/scripts/status.sh
+++ RaspberryPi/software/scripts/status.sh
@@ -1,2 +1,3 @@
 echo "Precharge: " `cat precharge`
 echo "Contactor: " `cat contactor`
+echo "BMS nFault: " `cat bms_nFault`
